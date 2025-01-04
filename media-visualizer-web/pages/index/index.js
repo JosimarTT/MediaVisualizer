1c23e60519612bc2f0feaa01b998c23bc0e4c2da
Sagadeath <sagadeath@hotmail.com>
2025-01-04 00:30:23 -0500

    index page finished

--- media-visualizer-web/pages/index/index.js
+++ media-visualizer-web/pages/index/index.js
@@ -4,9 +4,10 @@ let anime;
 let manga;
 let manwha;
 
+initialize().then(r => console.log('Initialized'));
+
 async function initialize() {
     [anime, manga, manwha] = await Promise.all([animeApi.getRandom(), mangaApi.getRandom(), manwhaApi.getRandom()]);
-
     let animeChapter = getRandomElement(anime.chapters);
     let manwhaLogo = getRandomElement(manwha.logos);
     let mangaChapter = getRandomElement(manga.chapters);
@@ -14,6 +15,3 @@ async function initialize() {
     document.getElementById('manga-card').querySelector('img').src = parseFilePath(manga.basePath, [mangaChapter.logo]);
     document.getElementById('manwha-card').querySelector('img').src = parseFilePath(manwha.basePath, [manwhaLogo]);
 }
-
-initialize().then(r => console.log('Initialized'));
-
