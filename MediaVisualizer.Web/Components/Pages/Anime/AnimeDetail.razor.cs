e5a0285650660dd391af28b4320a7a848b1a735e
Sagadeath <sagadeath@hotmail.com>
2025-03-25 19:32:23 -0500

    simple animedetail

--- MediaVisualizer.Web/Components/Pages/Anime/AnimeDetail.razor.cs
+++ MediaVisualizer.Web/Components/Pages/Anime/AnimeDetail.razor.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.JSInterop;
+﻿using MediaVisualizer.Services.Dtos;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace MediaVisualizer.Web.Components.Pages.Anime;
 
 public partial class AnimeDetail
 {
     private bool _isFirstRender = true;
+    [Parameter] public int AnimeId { get; set; }
+    private AnimeDto Anime { get; set; }
+    [Inject] private HttpClient HttpClient { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
@@ -15,13 +20,14 @@ public partial class AnimeDetail
         if (firstRender && _isFirstRender)
         {
             _isFirstRender = false;
-            await SetVideoSource();
+            Anime = await HttpClient.GetFromJsonAsync<AnimeDto>($"Anime/{AnimeId}");
+            await SetVideoSource($"{Anime.Folder}/{Anime.Video}");
         }
     }
 
-    private async Task SetVideoSource()
+    private async Task SetVideoSource(string path)
     {
-        var videoUrl = "http://localhost:5118/Anime/Stream";
+        var videoUrl = $"http://localhost:5118/Anime/Stream?path={path}";
         await JSRuntime.InvokeVoidAsync("setVideoSource", "animeVideo", videoUrl);
     }
 }
