505153dde5f24e7c42c03718ebf2141b2fbc533d
Sagadeath <sagadeath@hotmail.com>
2025-03-25 21:20:45 -0500

    add spinning component

---
+++ MediaVisualizer.Web/Components/Pages/Manga/MangaDetail.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MediaVisualizer.Web.Components.Pages.Manga;
+
+public partial class MangaDetail
+{
+    private const int pageSize = 5;
+
+    private readonly MangaDto mangaDto = new()
+    {
+        Folder = @"O\One Piece",
+        PagesCount = 43,
+        PageExtension = ".jpg"
+    };
+
+    private readonly List<string> mangaPages = new();
+    private int currentPage = 1;
+    [Parameter] public int MangaId { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await LoadMore();
+    }
+
+    private async Task LoadMore()
+    {
+        for (var i = 0; i < pageSize && currentPage <= mangaDto.PagesCount; i++, currentPage++)
+        {
+            var response =
+                await Http.GetAsync(
+                    $"Manga/GetMangaPage/page?folder={mangaDto.Folder}&page={currentPage}&extension={mangaDto.PageExtension}");
+            if (response.IsSuccessStatusCode)
+            {
+                var mangaPage = await response.Content.ReadAsStringAsync();
+                mangaPages.Add(mangaPage);
+            }
+        }
+    }
+
+    public class MangaDto
+    {
+        public string Folder { get; set; }
+        public int PagesCount { get; set; }
+        public string PageExtension { get; set; }
+    }
+}
