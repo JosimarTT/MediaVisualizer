8dafdcee27f3b54fe4724cd28a2020268bc5765d
Sagadeath <sagadeath@hotmail.com>
2025-03-26 23:04:15 -0500

    navigate with arrows in modal

--- MediaVisualizer.Web/Components/Pages/Manga/MangaDetail.razor.cs
+++ MediaVisualizer.Web/Components/Pages/Manga/MangaDetail.razor.cs
@@ -1,6 +1,8 @@
-﻿using MediaVisualizer.Services.Dtos;
+﻿using Blazorise;
+using MediaVisualizer.Services.Dtos;
 using MediaVisualizer.Shared;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace MediaVisualizer.Web.Components.Pages.Manga;
 
@@ -12,6 +14,10 @@ public partial class MangaDetail
     private bool _isFirstRender = true;
     private bool _isLoading = true;
     private int _totalPages = 1;
+    private int currentPage;
+    private string modalImageUrl;
+
+    private Modal modalRef;
     private MangaDto _manga { get; set; }
 
     [Parameter] public int MangaId { get; set; }
@@ -34,9 +40,14 @@ public partial class MangaDetail
             var filePath = Path.Combine(StringConstants.MangaCollectionPath, _manga.Folder,
                 $"{i:D3}{_manga.PageExtension}");
             var encodedFilePath = Uri.EscapeDataString(filePath);
-            var pageUrl =
-                $"{HttpClient.BaseAddress}FileStream/StreamImage?filePath={encodedFilePath}&percentage=20";
-            _pages.Add(new PageIsLoading { pagePath = pageUrl });
+
+            _pages.Add(new PageIsLoading
+            {
+                pageNumber = i,
+                pagePath = $"{HttpClient.BaseAddress}FileStream/StreamImage?filePath={encodedFilePath}&percentage=20",
+                pageFullPath =
+                    $"{HttpClient.BaseAddress}FileStream/StreamImage?filePath={encodedFilePath}&percentage=100"
+            });
         }
     }
 
@@ -46,9 +57,43 @@ public partial class MangaDetail
         await LoadPages(newPage);
     }
 
+    private Task ShowModal(PageIsLoading mangaPage)
+    {
+        modalImageUrl = mangaPage.pageFullPath;
+        currentPage = mangaPage.pageNumber;
+        return modalRef.Show();
+    }
+
+    private void OnKeyDown(KeyboardEventArgs e)
+    {
+        if (e.Key == "ArrowLeft")
+            ShowPreviousImage();
+        else if (e.Key == "ArrowRight") ShowNextImage();
+    }
+
+    private void ShowPreviousImage()
+    {
+        if (currentPage > 1)
+        {
+            currentPage--;
+            modalImageUrl = _pages.First(p => p.pageNumber == currentPage).pageFullPath;
+        }
+    }
+
+    private void ShowNextImage()
+    {
+        if (currentPage < _pages.Count)
+        {
+            currentPage++;
+            modalImageUrl = _pages.First(p => p.pageNumber == currentPage).pageFullPath;
+        }
+    }
+
     private class PageIsLoading
     {
+        public int pageNumber { get; set; }
         public string pagePath { get; set; }
+        public string pageFullPath { get; set; }
         public bool IsLoading { get; set; } = true;
     }
 }
