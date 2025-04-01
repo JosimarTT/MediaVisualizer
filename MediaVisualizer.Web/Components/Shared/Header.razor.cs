b20ff0a9dd98fe7d78554e3710ebb112101cf64e
Sagadeath <sagadeath@hotmail.com>
2025-04-01 00:15:28 -0500

    create reusable modal for filter

---
+++ MediaVisualizer.Web/Components/Shared/Header.razor.cs
@@ -0,0 +1,43 @@
+﻿namespace MediaVisualizer.Web.Components.Shared;
+
+public partial class Header
+{
+    private bool isModalVisible = false;
+    private List<string> modalItems;
+    private ModalList modalListRef;
+    private string modalTitle;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+    }
+
+    private async Task ShowModal(string title)
+    {
+        switch (title)
+        {
+            case "Tags":
+                modalTitle = title;
+                modalItems = DataHelper.Tags.Select(x => x.Name).ToList();
+                break;
+            case "Artists":
+                modalTitle = title;
+                modalItems = DataHelper.Artists.Select(x => x.Name).ToList();
+                break;
+            case "Brands":
+                modalTitle = title;
+                modalItems = DataHelper.Brands.Select(x => x.Name).ToList();
+                break;
+            default:
+                modalTitle = title;
+                modalItems = new List<string>();
+                break;
+        }
+
+        modalListRef.Show();
+    }
+
+    private bool IsCurrentPage(string pageName)
+    {
+        return Navigation.Uri.Contains(pageName, StringComparison.OrdinalIgnoreCase);
+    }
+}
---
+++ MediaVisualizer.Web/Components/Shared/ModalList.razor.cs
@@ -0,0 +1,25 @@
+﻿using Blazorise;
+using Microsoft.AspNetCore.Components;
+
+namespace MediaVisualizer.Web.Components.Shared;
+
+public partial class ModalList
+{
+    private Modal modalRef;
+    [Parameter] public string Title { get; set; }
+    [Parameter] public List<string> Items { get; set; }
+    [Parameter] public bool IsVisible { get; set; }
+    [Parameter] public EventCallback<bool> IsVisibleChanged { get; set; }
+
+    public async Task Show()
+    {
+        IsVisible = true;
+        await IsVisibleChanged.InvokeAsync(IsVisible);
+    }
+
+    private async Task Hide()
+    {
+        IsVisible = false;
+        await IsVisibleChanged.InvokeAsync(IsVisible);
+    }
+}
